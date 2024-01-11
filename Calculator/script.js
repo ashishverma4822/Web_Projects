6008f0bed011a406046fd1880cdae135de0172ac
ashishverma4822 <35832574+ashishverma4822@users.noreply.github.com>
2024-01-11 13:05:38 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -6,23 +6,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
   buttons.forEach(element => {
       element.addEventListener('click', (b) => {
-          if (b.target.innerText == '=') {
-              string = String(eval(string));
-              inputBox.value = string;
-          } else if (b.target.innerText == 'AC') {
-              string = '';
-              inputBox.value = string;
-              inputBox.style.fontSize = '40px';
-          } else if (b.target.innerText == 'DEL') {
-              string = string.substring(0, string.length - 1);
-              inputBox.value = string;
-          } else if (b.target.id == 'plusMinus') {
-              string = String(-eval(string));
-              inputBox.value = string;
-          } else {
-              string += b.target.innerText;
-              inputBox.value = string;
-              adjustFontSize();
+          try {
+              if (b.target.innerText == '=') {
+                  if (string.includes('0/0')) {
+                      handleError('Error: Division By Zero');
+                      return;
+                  }
+                  if (string.includes('/0')) {
+                      handleError('Error: Infinite Value');
+                      return;
+                  }
+                  string = String(eval(string));
+                  if (isNaN(string) || !isFinite(string)) {
+                      handleError('Error: Invalid expression');
+                      return;
+                  }
+                  inputBox.value = string;
+              } else if (b.target.innerText == 'AC') {
+                  clearInput();
+              } else if (b.target.innerText == 'DEL') {
+                  string = string.substring(0, string.length - 1);
+                  inputBox.value = string;
+              } else if (b.target.id == 'plusMinus') {
+                  string = String(-eval(string));
+                  inputBox.value = string;
+              } else {
+                  string += b.target.innerText;
+                  inputBox.value = string;
+                  adjustFontSize();
+              }
+          } catch (error) {
+              handleError('Error: Syntex Error');
           }
       });
   });
@@ -50,4 +64,17 @@ document.addEventListener('DOMContentLoaded', function () {
           inputBox.style.fontSize = '40px';
       }
   }
+
+  function handleError(errorMessage) {
+      inputBox.value = errorMessage;
+      inputBox.style.fontSize = '25px';
+      setTimeout(clearInput, 3000);
+  }
+
+  function clearInput() {
+      string = '';
+      inputBox.value = string;
+      inputBox.style.fontSize = '40px';
+  }
 });
+
